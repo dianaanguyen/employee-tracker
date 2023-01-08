146bcae2b2c96b17949a63409a197069c59c2deb
Diana Nguyen <diananguyen@Dianas-MBP-2.attlocal.net>
2023-01-07 18:47:02 -0800

    edited departments.js and added view and add function

--- db/departments.js
+++ db/departments.js
@@ -1,5 +1,33 @@
 const db = require("./db/connection");
+const { prompt, default: inquirer } = require("inquirer");
 
 async function viewAllDepartments() {
-    db.query('SELECT * FROM department').promise()
-}
+    try {
+        const department = 
+            await db.query ("SELECT * FROM department")
+        return departments
+    } catch (err) {
+        console.log(err)
+    }
+};
+
+async function addDepartment() {
+    try {
+        const departments = await viewAllDepartments();
+        const { name } =
+        await inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the name of the department you would like to add?"
+            }
+        ])
+        await db.query(`INSERT INTO department (name) VALUES ("${name}")`)
+        const addDepartment = await viewAllDepartments();
+        return addDepartment;
+    } catch (err) {
+        console.log(err);
+    };
+};
+
+module.exports = {viewAllDepartments, addDepartment};
